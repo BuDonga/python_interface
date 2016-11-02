8e0a29ab2a5c9d1539c3d85e0741b3a2c927dea4
BuDong <fish880111@163.com>
2016-11-02 17:44:22 +0800

    master

---
+++ b5c_interface/excelLoad.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+import xlrd
+
+
+class Excel:
+    def __init__(self, path='excel\\interface_caselist.xlsx'):
+        self.path = path
+
+    def open_excel(self):
+        try:
+            data = xlrd.open_workbook(self.path)
+            return data
+        except Exception, e:
+            print str(e)
+
+    def row_data(self, colnameindex=0, by_index=0):
+        """得到excel所有的行数据"""
+        data = self.open_excel()
+        table = data.sheets()[by_index]
+        nrows = table.nrows  # 行数
+        ncols = table.ncols  # 列数
+        colnames = table.row_values(colnameindex)
+        data_list = []
+        for i in range(1, nrows):
+            app = {}
+            n = 0
+            for a in table.row_values(i):
+                if a:
+                    app[colnames[n]] = a
+                    n += 1
+            data_list.append(app)
+        return data_list
+
+if __name__ == '__main__':
+    a = Excel()
+    print a .row_data()
+    print a.row_data()[0]['Method']
+    print a.row_data()[1]['Data']
+
---
+++ b5c_interface/mysql.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+import MySQLdb
+import ConfigParser
+
+
+class MySQL:
+    def __init__(self):
+        cf = ConfigParser.ConfigParser()
+        cf.read(r'..\db_config.ini')
+        self.host = cf.get('DATABASE1', 'host')
+        self.port = cf.get('DATABASE1', 'port')
+        self.user = cf.get('DATABASE1', 'user')
+        self.password = cf.get('DATABASE1', 'password')
+        self.db = cf.get('DATABASE1', 'db')
+        try:
+            self.db = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        except Exception, e:
+            print e
+            raise IOError('connection failed')
+        self.cursor = self.db.cursor()
+
+    def select(self, sql):
+        self.cursor.execute(sql)
+        print '受影响的行数一共有%d行' % self.cursor.rowcount
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.db.close()
+        print '-' * 50
+
+    def insert(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            print '受影响的行数一共有%d行' % self.cursor.rowcount
+            print '插入成功！！'
+        except Exception, e:
+            self.db.rollback()
+            print '插入失败！！'
+            print e
+
+    def update(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            print '受影响的行数一共有%d行' % self.cursor.rowcount
+            print '更新成功！！'
+        except Exception, e:
+            self.db.rollback()
+            print '更新失败！！'
+            print e
+
+    def delete(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            print '受影响的行数一共有%d行' % self.cursor.rowcount
+            print '删除成功！！'
+        except Exception, e:
+            self.db.rollback()
+            print '删除失败！！'
+            print e
+
+
+if __name__ == '__main__':
+    """select"""
+    a = MySQL()
+    sql = "SELECT * FROM test WHERE sex = 'm'"
+    try:
+        result = a.select(sql)
+        c = []
+        b = {}
+        index = 0
+        for re in result:
+            No = re[0]
+            name = re[1]
+            sex = re[2]
+            age = re[3]
+            c.append(name)
+            b[index] = re
+            index += 1
+            print No, name, sex, age
+    except Exception, e:
+        print e
+        raise IOError
+    finally:
+        a.close()
+    print c
+    print b
+
+    """insert"""
+    b = MySQL()
+    sql2 = "INSERT INTO `test` (`no`, `name`, `sex`, `age`) VALUES ('4', 'godda', 'male', '38')"
+    b.insert(sql2)
+    b.close()
+
+    """update"""
+    c = MySQL()
+    sql3 = "UPDATE `test` SET `sex`='m' WHERE (`sex`='male')"
+    c.update(sql3)
+    c.close()
+
+    """delete"""
+    d = MySQL()
+    sql4 = "DELETE FROM `test` WHERE no = '4'"
+    d.delete(sql4)
+    d.close()
